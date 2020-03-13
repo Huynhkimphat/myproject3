b5611cf70dabdda9c2d48c57251d945cf24c215b
Huynhkimphat <hkimphat2012@gmail.com>
2020-03-13 09:17:22 +0700

    Create HWb.cpp

---
+++ HWb/HWb.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+class INPUT
+{
+public:
+    int x;
+    double y;
+    string z;
+};
+int main() {
+    INPUT P;
+    ifstream myfile;
+    myfile.open("C:\\Users\\ASUS\\Documents\\GitHub\\myproject3\\HW\\1.txt");
+    myfile >> P.z >> P.x >> P.y;
+    cout << P.z << endl << P.x << endl << P.y ;
+    myfile.close();
+    
+    return 0;
+}
