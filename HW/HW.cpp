c2760cdabba3d1bc71c585eb5710242fb667b506
Huynhkimphat <hkimphat2012@gmail.com>
2020-03-13 09:16:40 +0700

    Update HW.cpp

--- HW/HW.cpp
+++ HW/HW.cpp
@@ -1,20 +1,32 @@
-// HW.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include<string>
 
-int main()
+using namespace std;
+class INPUT
 {
-    std::cout << "Hello World!\n";
+public:
+        int x;
+        double y;
+        string z;
+};
+int main() {
+    ofstream myfile;
+    myfile.open("1.txt");
+    int w;
+    cin >> w;
+    while (w != 0)
+    {
+        INPUT p;
+        cout << "Name:";
+        cin >> p.z;
+        cout << "Age: ";
+        cin >> p.x;
+        cout << "Avg: ";
+        cin >> p.y;
+        myfile << p.z << endl << p.x << endl << p.y << endl;
+        w--;
+    }
+    myfile.close();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
