0149fe31fcf2c89814a388f326289146a622378f
Huynhkimphat <hkimphat2012@gmail.com>
2020-03-13 09:19:21 +0700

    Create HWc.cpp

---
+++ HWc/HWc.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    int k = 0;
+    string line;
+    string find;
+    char name[25];
+    int id = 0;
+    float gpa = 0;
+
+    ofstream myfile;
+    myfile.open("data.txt");
+
+    while (k != 3) 
+    {
+        k = 0;
+        cout << "press 1 for adding data" << endl;
+        cout << "press 2 for update " << endl;
+        cin >> k;
+        if (k == 1)
+        {
+            cout << "enter ID " << endl;
+            cin >> id;
+
+            cout << "enter Name" << endl;
+            cin >> name;
+
+            cout << "enter GPA " << endl;
+            cin >> gpa;
+
+            myfile << name << endl;
+            myfile << id << endl;
+            myfile << gpa << endl;
+        }
+
+        if (k == 2)
+        {
+            cout << "name u want to update " << endl;
+            cin >> find;
+            ifstream file;
+
+            file.open("data.txt");
+            while (!file.eof() && line != find)
+            {
+                cout << "Yes";
+                getline(file, line);
+            }
+
+            cout << "enter ID " << endl;
+            cin >> id;
+
+            cout << "enter Name" << endl;
+            cin >> name;
+
+            cout << "enter GPA " << endl;
+            cin >> gpa;
+            myfile << name << endl;
+            myfile << id << endl;
+            myfile << gpa << endl << endl << endl;
+        }
+        if (k == 3) {
+            myfile.close();
+        }
+    }
+    
+    return 0;
+}
